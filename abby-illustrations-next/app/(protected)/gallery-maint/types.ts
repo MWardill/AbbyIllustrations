1da418388483d2cd05735eb270dfe8986f1a0707
MWardill <mat3740@gmail.com>
2026-01-05 19:06:49 +0000

    Fixes to generic routing

--- abby-illustrations-next/app/(protected)/gallery-maint/types.ts
+++ abby-illustrations-next/app/(protected)/gallery-maint/types.ts
@@ -1,7 +1,7 @@
 export interface Gallery {
   id: number;
   gallery_title: string;
-  menu_title: string;
+  menu_title: string | null;
   gallery_description: string;
   image_position: 'top' | 'center' | 'bottom' | 'left' | 'right' | null;
   image_count: number;
